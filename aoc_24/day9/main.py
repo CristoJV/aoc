715845e731961282f964f8967d97f7a257216aa9
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-01-25 14:08:17 +0100

    🔨 refactor(day9): Improve readability and simplify logic

--- aoc_24/day9/main.py
+++ aoc_24/day9/main.py
@@ -1,8 +1,9 @@
 # pylint: disable=C0114,C0116
 # pylint: disable=C0413,E0611
 import sys
+from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 utils_path = Path(__file__).resolve().parents[2] / "utils"
 sys.path.insert(0, str(utils_path))
@@ -22,23 +23,23 @@ def p1(lines: List[str]):
             empty_slots[i] = int(block_info)
     j = map_length - (2 if final_is_empty else 1)
     checksum = 0
-    block_index = 0
+    slot_index = 0
     for i in range(map_length):
         if (i % 2) == 0:
             value = sum(
                 map(
                     (i // 2).__mul__,
-                    range(block_index, block_index + data_blocks[i]),
+                    range(slot_index, slot_index + data_blocks[i]),
                 )
             )
             checksum += value
-            block_index = block_index + data_blocks[i]
+            slot_index = slot_index + data_blocks[i]
         if (i % 2) == 1 and j > i:
             break_flag_is_empty = False
             while empty_slots[i] > 0:
                 while data_blocks[j] > 0:
-                    checksum += j // 2 * block_index
-                    block_index += 1
+                    checksum += j // 2 * slot_index
+                    slot_index += 1
                     data_blocks[j] -= 1
                     empty_slots[i] -= 1
                     if empty_slots[i] < 1:
@@ -52,51 +53,62 @@ def p1(lines: List[str]):
     return checksum
 
 
-def get_slots_info(disc_map):
-    data_slots = {}
-    empty_slots = {}
-    block_index = 0
-    for i, block_slots in enumerate(disc_map):
-        block_slots = int(block_slots)
+@dataclass
+class BlockInfo:
+    id: int
+    slot_index: int
+    length: int
+
+
+def get_slots_info(
+    disc_map,
+) -> Tuple[List[BlockInfo], List[BlockInfo]]:
+    data_slots = []
+    empty_slots = []
+    slot_index = 0
+    for i, length in enumerate(disc_map):
+        length = int(length)
         if (i % 2) == 0:
-            data_slots[i // 2] = [block_index, block_slots]
-            block_index += block_slots
+            data_slots.append(BlockInfo(i // 2, slot_index, length))
         if (i % 2) == 1:
-            empty_slots[i // 2] = [block_index, block_slots]
-            block_index += block_slots
+            empty_slots.append(BlockInfo(0, slot_index, length))
+        slot_index += length
     return data_slots, empty_slots
 
 
 def p2(lines: List[str]):
-
     data_slots, empty_slots = get_slots_info(lines[0].strip())
-    for data_id, data_slot in sorted(data_slots.items(), reverse=True):
-        # pylint: disable=C0201,C0206
-        for empty_slot_idx in empty_slots.keys():
-            empty_slot = empty_slots[empty_slot_idx]
-            if data_slot[0] < empty_slot[0]:
+
+    for _, data_slot in reversed(list(enumerate(data_slots))):
+        for empty_slot_idx, empty_slot in enumerate(empty_slots):
+            if data_slot.slot_index < empty_slot.slot_index:
                 break
-            if data_slot[1] <= empty_slot[1]:
+
+            if data_slot.length <= empty_slot.length:
                 # Update the data indexes
-                data_slot[0] = empty_slot[0]
+                data_slot.slot_index = empty_slot.slot_index
                 # Update the empty_slot indexes and len
-                rest_slots = empty_slot[1] - data_slot[1]
-                if rest_slots <= 0:
-                    # print(f"Deleting empty slot {empty_slot_idx}")
+                remaining_length = empty_slot.length - data_slot.length
+                if remaining_length <= 0:
                     empty_slots.pop(empty_slot_idx)
                 else:
-                    new_block_index = empty_slot[0] + data_slot[1]
-                    empty_slots[empty_slot_idx] = (
-                        new_block_index,
-                        rest_slots,
+                    new_slot_index = empty_slot.slot_index + data_slot.length
+                    empty_slots[empty_slot_idx] = BlockInfo(
+                        0,
+                        new_slot_index,
+                        remaining_length,
                     )
                 break
     checksum = 0
-    for data_id, data_slot in data_slots.items():
+
+    for data_slot in data_slots:
         value = sum(
             map(
-                (data_id).__mul__,
-                range(data_slot[0], data_slot[0] + data_slot[1]),
+                (data_slot.id).__mul__,
+                range(
+                    data_slot.slot_index,
+                    data_slot.slot_index + data_slot.length,
+                ),
             )
         )
         checksum += value
