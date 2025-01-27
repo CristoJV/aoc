67629384f0f55d0cd43188ba52652a58381178f5
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-01-27 20:01:08 +0100

    🧩 solve(day11): Plutonian Pebbles (Part 2)
    Using Memoization

--- aoc_24/day11/main.py
+++ aoc_24/day11/main.py
@@ -2,11 +2,10 @@
 # pylint: disable=C0413,E0611
 import sys
 from pathlib import Path
-from typing import List
+from typing import Dict, List
 
 utils_path = Path(__file__).resolve().parents[2] / "utils"
 sys.path.insert(0, str(utils_path))
-from profile import timeit
 
 
 def parse_stones(lines: List[str]) -> List[str]:
@@ -14,42 +13,69 @@ def parse_stones(lines: List[str]) -> List[str]:
     return stones
 
 
-def blink(stone: str, blink_step, max_steps=1) -> List[str]:
-    if blink_step == max_steps:
-        return [stone]
+def blink(
+    stone: str, blink_step, memo: Dict[str, List[int]], max_steps=1
+) -> int:
+    if blink_step >= max_steps:
+        memo.setdefault(stone, [0] * max_steps)[blink_step - 1] = 1
+        return 1
+
+    if stone in memo:
+        stone_count_in_memo = memo[stone][blink_step - 1]
+        if stone_count_in_memo != 0:
+            return stone_count_in_memo
 
     if all(digit == "0" for digit in list(stone)):
-        return blink("1", blink_step + 1, max_steps)
+        exploded_stone = "1"
+        stone_count = blink(
+            stone=exploded_stone,
+            blink_step=blink_step + 1,
+            memo=memo,
+            max_steps=max_steps,
+        )
+        memo.setdefault(stone, [0] * max_steps)[blink_step - 1] = stone_count
+        return stone_count
 
     if len(stone) % 2 == 0:
         left_half = len(stone) // 2
         exploted_stone_first_half = stone[:left_half]
         exploded_stone_second_half = str(int(stone[left_half:]))
-        stones_first_half = blink(
-            exploted_stone_first_half, blink_step + 1, max_steps
+        stone_count = blink(
+            exploted_stone_first_half,
+            blink_step + 1,
+            memo=memo,
+            max_steps=max_steps,
+        ) + blink(
+            exploded_stone_second_half,
+            blink_step + 1,
+            memo=memo,
+            max_steps=max_steps,
         )
-        stones_second_half = blink(
-            exploded_stone_second_half, blink_step + 1, max_steps
-        )
-        stones_first_half.extend(stones_second_half)
-        return stones_first_half
+        memo.setdefault(stone, [0] * max_steps)[blink_step - 1] = stone_count
+        return stone_count
 
     exploded_stone = str(int(stone) * 2024)
-    return blink(exploded_stone, blink_step + 1, max_steps)
+    stone_count = blink(
+        exploded_stone, blink_step + 1, memo=memo, max_steps=max_steps
+    )
+    memo.setdefault(stone, [0] * max_steps)[blink_step - 1] = stone_count
+    return stone_count
 
 
 def p1(lines: List[str]):
     stones = parse_stones(lines)
-    exploded_stones = [
-        element
-        for stone in stones
-        for element in blink(stone, blink_step=0, max_steps=25)
-    ]
-    return len(exploded_stones)
+    count = sum(
+        [blink(stone, blink_step=0, memo={}, max_steps=25) for stone in stones]
+    )
+    return count
 
 
 def p2(lines: List[str]):
-    pass
+    stones = parse_stones(lines)
+    count = sum(
+        [blink(stone, blink_step=0, memo={}, max_steps=75) for stone in stones]
+    )
+    return count
 
 
 if __name__ == "__main__":
