800e7be47145573b737ac27e22ef78b51d75a137
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-01-27 20:36:25 +0100

    ⚡perf(day11): Add `lru_cache` to improve performance of `blink` fuction
    Integrated `functools.lru_cache` to optimize recursive calls in `solve`.
    
    - First part:
      - Without `lru_cache`: 0.010962 seconds (avg).
      - With `lru_cache`: 0.004640 seconds (avg).
    
    - Second part:
      - Without `lru_cache`: 0.518932 seconds (avg).
      - With `lru_cache`: 0.177294 seconds (avg).

--- aoc_24/day11/main.py
+++ aoc_24/day11/main.py
@@ -1,11 +1,14 @@
 # pylint: disable=C0114,C0116
 # pylint: disable=C0413,E0611
 import sys
+from functools import lru_cache
 from pathlib import Path
 from typing import Dict, List
 
 utils_path = Path(__file__).resolve().parents[2] / "utils"
 sys.path.insert(0, str(utils_path))
+
+from profile import timeit
 
 
 def parse_stones(lines: List[str]) -> List[str]:
@@ -13,69 +16,74 @@ def parse_stones(lines: List[str]) -> List[str]:
     return stones
 
 
+def expanded_stones(stone: str) -> List[str]:
+    stone_int = int(stone)
+    if stone_int == 0:
+        return ["1"]
+    elif len(stone) % 2 == 0:
+        half = len(stone) // 2
+        return [stone[:half], str(int(stone[half:]))]
+    else:
+        return [str(stone_int * 2024)]
+
+
 def blink(
     stone: str, blink_step, memo: Dict[str, List[int]], max_steps=1
 ) -> int:
     if blink_step >= max_steps:
-        memo.setdefault(stone, [0] * max_steps)[blink_step - 1] = 1
         return 1
 
-    if stone in memo:
-        stone_count_in_memo = memo[stone][blink_step - 1]
-        if stone_count_in_memo != 0:
-            return stone_count_in_memo
+    if stone not in memo:
+        memo[stone] = [0] * max_steps
 
-    if all(digit == "0" for digit in list(stone)):
-        exploded_stone = "1"
-        stone_count = blink(
-            stone=exploded_stone,
+    cached_stone_count = memo[stone][blink_step]
+    if memo[stone][blink_step] != 0:
+        return cached_stone_count
+
+    stone_count = sum(
+        blink(
+            expanded_stone,
             blink_step=blink_step + 1,
             memo=memo,
             max_steps=max_steps,
         )
-        memo.setdefault(stone, [0] * max_steps)[blink_step - 1] = stone_count
-        return stone_count
-
-    if len(stone) % 2 == 0:
-        left_half = len(stone) // 2
-        exploted_stone_first_half = stone[:left_half]
-        exploded_stone_second_half = str(int(stone[left_half:]))
-        stone_count = blink(
-            exploted_stone_first_half,
-            blink_step + 1,
-            memo=memo,
-            max_steps=max_steps,
-        ) + blink(
-            exploded_stone_second_half,
-            blink_step + 1,
-            memo=memo,
-            max_steps=max_steps,
-        )
-        memo.setdefault(stone, [0] * max_steps)[blink_step - 1] = stone_count
-        return stone_count
-
-    exploded_stone = str(int(stone) * 2024)
-    stone_count = blink(
-        exploded_stone, blink_step + 1, memo=memo, max_steps=max_steps
+        for expanded_stone in expanded_stones(stone)
     )
-    memo.setdefault(stone, [0] * max_steps)[blink_step - 1] = stone_count
+    memo[stone][blink_step] = stone_count
     return stone_count
 
 
-def p1(lines: List[str]):
+@timeit(repeats=5)
+def solve(lines: List[str], max_steps: int):
     stones = parse_stones(lines)
     count = sum(
-        [blink(stone, blink_step=0, memo={}, max_steps=25) for stone in stones]
+        blink(stone, blink_step=0, memo={}, max_steps=max_steps)
+        for stone in stones
     )
     return count
+
+
+@timeit(repeats=5)
+def solve_with_lru_cache(lines: List[str], max_steps: int):
+    @lru_cache(None)
+    def blink_with_lru(stone: str, step: int) -> int:
+        if step >= max_steps:
+            return 1
+        return sum(
+            blink_with_lru(expanded_stone, step + 1)
+            for expanded_stone in expanded_stones(stone)
+        )
+
+    stones = parse_stones(lines)
+    return sum(blink_with_lru(stone, 0) for stone in stones)
+
+
+def p1(lines: List[str]):
+    return solve(lines, 25), solve_with_lru_cache(lines, 25)
 
 
 def p2(lines: List[str]):
-    stones = parse_stones(lines)
-    count = sum(
-        [blink(stone, blink_step=0, memo={}, max_steps=75) for stone in stones]
-    )
-    return count
+    return solve(lines, 75), solve_with_lru_cache(lines, 75)
 
 
 if __name__ == "__main__":
