e84f5a8a52ff19c49844aae5d9a2bbb16ad8b560
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-02-02 20:26:29 +0100

    🧩 solve(day14) : Restroom Redoubt (Part 2)
    - Applied a custom 2D entropy analysis to detect the hidden christmas tree.
    - Computed the shannon entropy using a kernel-based approach (center, top, left, bottom)

--- aoc_24/day14/main.py
+++ aoc_24/day14/main.py
@@ -3,6 +3,7 @@
 import math
 import re
 import sys
+from collections import defaultdict
 from dataclasses import dataclass
 from functools import reduce
 from operator import mul
@@ -11,7 +12,6 @@ from typing import List
 
 utils_path = Path(__file__).resolve().parents[2] / "utils"
 sys.path.insert(0, str(utils_path))
-from profile import timeit
 
 
 @dataclass
@@ -38,8 +38,9 @@ def step(robot_kinematic: RobotKinematic, t: int, width: int, height: int):
     return new_x, new_y
 
 
-def p1(lines: List[str], t: int, width: int, height: int):
-    robot_kinematics = parse_robot_kinematics(lines)
+def get_safety_factor(
+    robot_kinematics: List[RobotKinematic], t: int, width: int, height: int
+):
     quadrant_count = {}
     for robot_kinematic in robot_kinematics:
         new_x, new_y = step(robot_kinematic, t, width, height)
@@ -50,12 +51,67 @@ def p1(lines: List[str], t: int, width: int, height: int):
             quadrant_count[quadrant] = (
                 quadrant_count.setdefault(quadrant, 0) + 1
             )
-    result = reduce(mul, quadrant_count.values())
-    return result
+    safety_factor = reduce(mul, quadrant_count.values())
+    return safety_factor
 
 
-def p2(lines: List[str]):
-    pass
+def p1(lines: List[str], t: int, width: int, height: int):
+    robot_kinematics = parse_robot_kinematics(lines)
+    return get_safety_factor(robot_kinematics, t, width, height)
+
+
+def generate_map(robot_kinematics, t, width, height):
+    robot_map = [[0 for _ in range(width)] for _ in range(height)]
+    for robot_kinematic in robot_kinematics:
+        new_x, new_y = step(robot_kinematic, t, width, height)
+        robot_map[new_y][new_x] = 1
+    return robot_map
+
+
+def p2(lines: List[str], width: int, height: int):
+    robot_kinematics = parse_robot_kinematics(lines)
+
+    # Asuming the Christmas tree is in one of the quadrants. In that
+    # case, the safety factor should be the loweset
+
+    # min_sf: int = math.inf
+    # best_t: int = 0
+    # for t in range(0, width * height):
+    #     sf = get_safety_factor(robot_kinematics, t, width, height)
+    #     if sf < min_sf:
+    #         min_sf = sf
+    #         best_t = t
+
+    # The minimum safety factor does not provide any good solution
+
+    # Assuming lower joint entropy for both vertical and horizontal
+    def compute_joint_entropy(robot_map):
+        joint_hist = defaultdict(int)
+        for y in range(1, len(robot_map) - 1):
+            for x in range(1, len(robot_map[0]) - 1):
+                center = robot_map[y][x]
+                top = robot_map[y - 1][x]
+                right = robot_map[y][x + 1]
+                bot = robot_map[y + 1][x]
+                left = robot_map[y][x - 1]
+                pattern = (center, top, right, bot, left)
+                joint_hist[pattern] += 1
+
+        total_patterns = sum(joint_hist.values())
+        joint_prob = {k: v / total_patterns for k, v in joint_hist.items()}
+        entropy = -sum(p * math.log2(p + 13 - 10) for p in joint_prob.values())
+        return entropy
+
+    min_entropy: int = math.inf
+    best_t: int = 0
+    for t in range(0, width * height):
+        robot_map = generate_map(robot_kinematics, t, width, height)
+        entropy = compute_joint_entropy(robot_map)
+        if entropy < min_entropy:
+            min_entropy = entropy
+            best_t = t
+
+    return best_t
 
 
 if __name__ == "__main__":
@@ -65,4 +121,4 @@ if __name__ == "__main__":
     ) as f:
         input_lines = f.readlines()
         print(f"First part: {p1(input_lines, t=100, width=101, height=103)}")
-        print(f"Second part: {p2(input_lines)}")
+        print(f"Second part: {p2(input_lines, width=101, height=103)}")
