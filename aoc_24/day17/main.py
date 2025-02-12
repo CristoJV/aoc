e8056d6b2d4d4646ab24692be197b3d8f53d0af2
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-02-12 16:47:04 +0100

    🧩 solve(day17): Chronospatial Computer (Part 2)
    DFS exploration considering octal increments

--- aoc_24/day17/main.py
+++ aoc_24/day17/main.py
@@ -3,7 +3,7 @@
 import re
 import sys
 from pathlib import Path
-from typing import Dict, List
+from typing import List
 
 utils_path = Path(__file__).resolve().parents[2] / "utils"
 sys.path.insert(0, str(utils_path))
@@ -15,80 +15,97 @@ def parse_debugger(lines: List[str]):
     matches = re.findall(pattern, full_str)
     a, b, c, program = matches[0]
     program = list(map(int, program.split(",")))
-    return a, b, c, program
+    return int(a), int(b), int(c), program
 
 
 def p1(lines: List[str]):
     a, b, c, program = parse_debugger(lines)
-    pointer_ref = [0]
     output = []
 
-    a_ref = [int(a)]
-    b_ref = [int(b)]
-    c_ref = [int(c)]
-
     # Using mutable operands
-    operands: Dict[int, List[int]] = {
-        0: [0],
-        1: [1],
-        2: [2],
-        3: [3],
-        4: a_ref,
-        5: b_ref,
-        6: c_ref,
-        7: [0],
-    }
+    operands: List[int] = [0, 1, 2, 3, a, b, c, -1]
+    pointer: int = 0
 
-    def adv(x: int):
-        x = operands[x][0]
-        a_ref[0] = int(a_ref[0] / (2 ** (x)))
-
-    def bxl(x: int):
-        b_ref[0] = b_ref[0] ^ x
-
-    def bst(x: int):
-        x = operands[x][0]
-        b_ref[0] = x % 8
-
-    def jnz(x: int):
-        if a_ref[0] != 0:
-            pointer_ref[0] = x - 2
-
-    def bxc(x: int):
-        b_ref[0] = b_ref[0] ^ c_ref[0]
-
-    def out(x: int):
-        x = operands[x][0]
-        output.append(x % 8)
-
-    def bdv(x: int):
-        x = operands[x][0]
-        b_ref[0] = int(a_ref[0] / (2 ** (x)))
-
-    def cdv(x: int):
-        x = operands[x][0]
-        c_ref[0] = int(a_ref[0] / (2 ** (x)))
-
-    opcodes: Dict[int, callable] = {
-        0: adv,
-        1: bxl,
-        2: bst,
-        3: jnz,
-        4: bxc,
-        5: out,
-        6: bdv,
-        7: cdv,
-    }
-
-    while pointer_ref[0] < len(program):
-        opcodes[program[pointer_ref[0]]](program[pointer_ref[0] + 1])
-        pointer_ref[0] += 2
-
+    while pointer < len(program):
+        operator = program[pointer]
+        x = program[pointer + 1]
+        match operator:
+            case 0:  # adv
+                operands[4] = int(operands[4] / (2 ** (operands[x])))
+            case 1:  # bxl
+                operands[5] = operands[5] ^ x
+            case 2:  # bst
+                operands[5] = operands[x] % 8
+            case 3:  # jnz
+                if operands[4] != 0:
+                    pointer = x - 2
+            case 4:  # bxc
+                operands[5] = operands[5] ^ operands[6]
+            case 5:  # out
+                output.append(operands[x] % 8)
+            case 6:  # bdv
+                operands[5] = int(operands[4] / (2 ** (operands[x])))
+            case 7:  # cdv
+                operands[6] = int(operands[4] / (2 ** (operands[x])))
+        pointer += 2
     return ",".join(map(str, output))
 
 
+def get_output(a, program):
+    output = []
+    operands: List[int] = [0, 1, 2, 3, a, 0, 0, -1]
+    pointer: int = 0
+    while pointer < len(program):
+        operator = program[pointer]
+        x = program[pointer + 1]
+        match operator:
+            case 0:  # adv
+                operands[4] = int(operands[4] / (2 ** (operands[x])))
+            case 1:  # bxl
+                operands[5] = operands[5] ^ x
+            case 2:  # bst
+                operands[5] = operands[x] % 8
+            case 3:  # jnz
+                if operands[4] != 0:
+                    pointer = x - 2
+            case 4:  # bxc
+                operands[5] = operands[5] ^ operands[6]
+            case 5:  # out
+                output.append(operands[x] % 8)
+            case 6:  # bdv
+                operands[5] = int(operands[4] / (2 ** (operands[x])))
+            case 7:  # cdv
+                operands[6] = int(operands[4] / (2 ** (operands[x])))
+        pointer += 2
+    return output
+
+
+def dfs_exploration(a: int, depth: int, program: List[int]):
+    output = get_output(
+        a,
+        program,
+    )
+
+    if depth == len(program):
+        if output == program:
+            return True, a
+        else:
+            return False, 0
+    if output == program[-depth:]:  # Continue one depth
+        a *= 8
+        for i in range(8):
+            status, total_a = dfs_exploration(a + i, depth + 1, program)
+            if status:
+                return True, total_a
+    return False, 0
+
+
 def p2(lines: List[str]):
-    pass
+    a, _, _, program = parse_debugger(lines)
+    for a in range(8):
+        status, resulting_a = dfs_exploration(a, 1, program)
+        if status:
+            print(resulting_a)
 
 
 if __name__ == "__main__":
