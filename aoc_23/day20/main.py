1d2e949deafda1764f9f27ebbc1f5d3efe27dc9e
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-02-13 00:14:28 +0100

    🧩 solve(day20): Pulse Propagation (Part 1)

--- aoc_23/day20/main.py
+++ aoc_23/day20/main.py
@@ -1,150 +1,123 @@
 import functools
 import logging
-from enum import Enum
+import math
+from collections import deque
+from dataclasses import dataclass
+from typing import List
 
 
-class Signal(Enum):
-    LOW = 0
-    HIGH = 1
-
-
-class Broadcast:
-    def __init__(self):
-        self.count_low_pulses = 0
-        self.count_high_pulses = 0
-
-        self.outputs = []
-
-    def connect_output(self, output):
-        self.outputs.append(output)
-
-    def update(self, input, pulse):
-        if pulse == 0:
-            self.count_low_pulses += 1
-        else:
-            self.count_high_pulses += 1
-        return [
-            functools.partial(output.update, self, Signal.LOW)
-            for output in self.outputs
-        ]
-
-
-class FlipFlop:
-    def __init__(self):
-        self.outputs = []
-        self._state = False
-        self.count_low_pulses = 0
-        self.count_high_pulses = 0
-        self.outputs = []
-
-    def connect_output(self, output):
-        self.outputs.append(output)
-
-    def update(self, input, pulse):
-        if pulse == Signal.HIGH:  # Don't update the state if the pulse is HIGH
-            self.count_high_pulses += 1
-        elif pulse == Signal.LOW:  # Update the state if the pulse is LOW
-            self._state = not self._state
-            self.count_low_pulses += 1
-            return [
-                functools.partial(output.update, self, self._state)
-                for output in self.outputs
-            ]
-
-
-class Conjuction:
-    def __init__(self):
-        self.outputs = {}
-        self.count_low_pulses = 0
-        self.count_high_pulses = 0
-
-    def connect_output(self, output):
-        self.outputs[output] = Signal.LOW
-
-    def update(self, input, pulse):
-        self.outputs[input] = pulse
-        if pulse == 0:
-            self.count_low_pulses += 1
-        else:
-            self.count_high_pulses += 1
-
-        return [
-            functools.partial(output.update, self, self.send())
-            for output in self.outputs
-        ]
-
-    def send(self):
-        return int(not all(self.outputs.values()))
-
-
-def parse_lines(lines):
+def parse_circuit(lines):
     modules = [line.strip().split(" -> ") for line in lines]
     modules_ = {}
     for module in modules:
         modules_[module[0]] = module[1].split(", ")
     modules = modules_
-    return modules
+
+    def custom_key(key):
+        if key[0].isalpha():
+            return (0, key)
+        else:
+            return (1, key)
+
+    modules = dict(sorted(modules.items(), key=lambda x: custom_key(x[0])))
+    modules_dict = {}
+
+    broadcasted_modules = []
+
+    for module_name, out_modules in modules.items():
+        module_type, module_name = module_name[0], module_name[1:]
+
+        if module_type == "%":
+            modules_dict[module_name] = Flipflop(module_name, out_modules)
+
+        elif module_type == "&":
+            modules_dict[module_name] = Conjuntion(module_name, out_modules)
+
+            for module_name_i, out_modules_i in modules.items():
+                for out_module in out_modules_i:
+                    if out_module == module_name:
+                        modules_dict[module_name].add_in_module(
+                            module_name_i[1:]
+                        )
+        else:
+            broadcasted_modules = out_modules
+    return modules_dict, broadcasted_modules
+
+
+class SignalState:
+    LOW = 0
+    HIGH = 1
+
+
+@dataclass
+class Signal:
+    sender: str
+    receiver: str
+    state: SignalState
+
+
+class Flipflop:
+    def __init__(self, name: str, out_modules: List[str]):
+        self.name = name
+        self.state = "off"
+        self.out_modules = out_modules
+
+    def update(self, signal: Signal):
+        signals = []
+        if signal.state == SignalState.LOW:
+            self.state = "off" if self.state == "on" else "on"
+            signal_state = (
+                SignalState.HIGH if self.state == "on" else SignalState.LOW
+            )
+            for out_module in self.out_modules:
+                signals.append(Signal(self.name, out_module, signal_state))
+        return signals
+
+
+class Conjuntion:
+    def __init__(self, name: str, out_modules: List[str]):
+        self.name = name
+        self.in_modules = {}
+        self.out_modules = out_modules
+
+    def add_in_module(self, in_module: str):
+        self.in_modules[in_module] = SignalState.LOW
+
+    def update(self, signal: Signal):
+        signals = []
+        self.in_modules[signal.sender] = signal.state
+        if all(
+            value == SignalState.HIGH for value in self.in_modules.values()
+        ):
+            signal_state = SignalState.LOW
+        else:
+            signal_state = SignalState.HIGH
+        for out_module in self.out_modules:
+            signals.append(Signal(self.name, out_module, signal_state))
+        return signals
 
 
 def part_1(lines):
-    modules = parse_lines(lines)
-    modules_dict = {}
-    aliases = {}
+    modules, broascast = parse_circuit(lines)
+    queue = deque()
+    low_pulses = 0
+    high_pulses = 0
+    for _ in range(1000):
+        low_pulses += 1
+        for init in broascast:
+            queue.append(Signal("broadcast", init, SignalState.LOW))
 
-    for input_module, _ in modules.items():
-        if input_module[0] == "%":
-            modules_dict[input_module] = FlipFlop()
-        elif input_module[0] == "&":
-            modules_dict[input_module] = Conjuction()
-        else:
-            modules_dict[input_module] = Broadcast()
-        aliases[input_module[1:]] = input_module
-        aliases[input_module] = input_module
-
-    for input_module, outputs in modules.items():
-        for output in outputs:
-            if output not in aliases:
-                aliases[output] = output
-                modules_dict[output] = Broadcast()
-
-            modules_dict[input_module].connect_output(
-                modules_dict[aliases[output]]
-            )
-            aliases[output] = aliases[input_module]
-
-    start_module = "broadcaster"
-    pending_signals = modules_dict[start_module].update(
-        start_module, Signal.HIGH
-    )
-
-    print(pending_signals)
-
-    while len(pending_signals) > 0:
-        for signal in pending_signals:
-            followed_signals = signal()
-            pending_signals.remove(signal)
-            if followed_signals is not None:
-                pending_signals.extend(followed_signals)
-
-    # for input, outputs in modules.items():
-    #     print(f"module {input}")
-    #     for output in outputs:
-    #         if output not in aliases:
-    #             aliases[output] = output
-    #             modules_dict[output] = Broadcast()
-
-    #         print(output)
-    #         print(aliases[output], aliases[input])
-    #         modules_dict[aliases[output]].update(
-    #             input, modules_dict[aliases[input]].send()
-    #         )
-
-    total_low_pulses = 0
-    total_high_pulses = 0
-    for module in modules_dict.values():
-        total_low_pulses += module.count_low_pulses
-        total_high_pulses += module.count_high_pulses
-    print(total_low_pulses, total_high_pulses)
+        while queue:
+            signal = queue.popleft()
+            if signal.state == SignalState.HIGH:
+                high_pulses += 1
+            else:
+                low_pulses += 1
+            if signal.receiver in modules:
+                signals = modules[signal.receiver].update(signal)
+                for sig in signals:
+                    queue.append(sig)
+    return low_pulses * high_pulses
 
 
 def part_2(lines):
@@ -152,7 +125,7 @@ def part_2(lines):
 
 
 if __name__ == "__main__":
-    with open("example_input.txt") as f:
+    with open("input.txt") as f:
         lines = f.readlines()
 
     logging.basicConfig(level=logging.INFO)
