bdfdd7d70a391908ab8d4cbd79401a909adc0994
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-02-15 17:51:33 +0100

    🧩 solve(day22): Monkey Market (Part 2)

--- aoc_24/day22/main.py
+++ aoc_24/day22/main.py
@@ -3,7 +3,9 @@
 import math
 import sys
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Tuple
+
+import numpy as np
 
 utils_path = Path(__file__).resolve().parents[2] / "utils"
 sys.path.insert(0, str(utils_path))
@@ -35,7 +37,44 @@ def p1(lines: List[str]):
 
 
 def p2(lines: List[str]):
-    pass
+    memo = {}
+    price_changes: Dict[int, List[int]] = {}
+    for buyer in parse_secrets(lines):
+        secret = buyer
+        price_changes.setdefault(buyer, []).append(buyer % 10)
+        for _ in range(2000):
+            secret = get_next_secret(secret, memo)
+            price_changes[buyer].append(secret % 10)
+
+    sequences: List[Dict[Tuple[int, int, int, int], int]] = []
+
+    for buyer, buyer_price_changes in price_changes.items():
+        buyer_price_changes = np.asarray(buyer_price_changes)
+        buyer_sequences: Dict[Tuple[int, int, int, int], int] = {}
+        diff = buyer_price_changes[1:] - buyer_price_changes[:-1]
+
+        for i in range(4, len(buyer_price_changes)):
+            sequence = tuple(diff[i - 4 : i].tolist())
+            if sequence not in buyer_sequences:
+                buyer_sequences[sequence] = int(buyer_price_changes[i])
+
+        sequences.append(buyer_sequences)
+
+    merge_sequences: Dict[Tuple[int, int, int, int], int] = {}
+    for buyer_sequences in sequences:
+        for sequence, price in buyer_sequences.items():
+            merge_sequences.setdefault(sequence, 0)
+            merge_sequences[sequence] += price
+    best_sequence = next(
+        iter(
+            sorted(
+                merge_sequences.items(),
+                key=lambda item: item[1],
+                reverse=True,
+            ),
+        )
+    )
+    return best_sequence
 
 
 if __name__ == "__main__":
